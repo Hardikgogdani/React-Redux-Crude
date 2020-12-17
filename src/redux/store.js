898f29d750c78c8090c279134376a7d368d9ea26
hardik gogdani <hardikgogdani989@gmail.com>
2020-12-17 17:39:23 +0530

    change

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
 import userReducer from './User/userReducer';
 import logger from 'redux-logger';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-const store =createStore(userReducer,compose(applyMiddleware(logger),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const persistedState = localStorage.getItem('reduxState')
+    ? JSON.parse(localStorage.getItem('reduxState'))
+    : {
+        data: []
+    }
+const store = createStore(userReducer, persistedState, compose(applyMiddleware(logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
+store.subscribe(() => {
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 export default store;
