f1160a6ca0e9cb605556c83ff0696c9a470c7a92
hardik gogdani <hardikgogdani989@gmail.com>
2020-12-17 15:29:01 +0530

    change

--- src/component/userContainer.js
+++ src/component/userContainer.js
@@ -12,6 +12,7 @@ const UserContainer = (props) => {
     const [userDetail, setUserDetail] = useState({});
     const [editableIndex, setIndex] = useState(null);
     const [data, setData] = useState([]);
+    const [error, setError] = useState({});
 
     useEffect(() => {
         setData(list)
@@ -31,15 +32,61 @@ const UserContainer = (props) => {
         setUserDetail({ ...userDetail, [name]: value })
     }
 
+    const validate = (name, value) => {
+        const emailRegx = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/ig;
+        const numRegx = /^\d{1,6}(?:\.\d{0,2})?$/g;
+        switch (name) {
+            case 'firstName':
+                if (!value) return "First Name is required";
+                return null;
+            case 'lastName':
+                if (!value) return "Last Name is required";
+                return null;
+            case 'email':
+                if (!emailRegx.test(value)) return "Email is required";
+                return null;
+            case 'age':
+                if (!numRegx.test(value)) return "Age is required";
+                return null;
+            case 'gender':
+                if (!value) return "Gender is required";
+                return null;
+            case 'password':
+                if (!value) return "Password is required";
+                return null;
+            default:
+                return null;
+        }
+    };
     const onSubmit = () => {
-        if (editableIndex !== null) {
-            userUpdates(userDetail)
-            setIndex(null)
-            setUserDetail({})
+        let errorObj = {}
+        const newsUerDetail = {
+            firstName: userDetail.firstName,
+            lastName: userDetail.lastName,
+            email: userDetail.email,
+            age: userDetail.age,
+            gender: userDetail.gender,
+            password: userDetail.password
+        }
+
+        Object.keys(newsUerDetail).forEach((key) => {
+            const error = validate(key, newsUerDetail[key]);
+            if (error && error.length) {
+                errorObj[key] = error;
+            }
+        });
+        if (Object.keys(errorObj).length) {
+            return setError(errorObj);
         } else {
-            userDetail.id = data.length + 1;
-            UserDatas(userDetail)
-            setUserDetail({})
+            if (editableIndex !== null) {
+                userUpdates(userDetail)
+                setIndex(null)
+                setUserDetail({})
+            } else {
+                userDetail.id = data.length + 1;
+                UserDatas(userDetail)
+                setUserDetail({})
+            }
         }
     }
 
@@ -57,26 +104,26 @@ const UserContainer = (props) => {
                                 <Input placeholder="Enter Your firstname" name="firstName"
                                     value={userDetail.firstName}
                                     onChange={handleChange} addonBefore={(<UserOutlined />)} />
-
+                                <span className="danger">{error.firstName || ""}</span>
                             </Form.Item>
 
                             <Form.Item>
                                 <Input placeholder="Enter Your lastname" name="lastName" value={userDetail.lastName}
                                     onChange={handleChange} addonBefore={(<UserOutlined />)} />
-
+                                <span className="danger">{error.lastName || ""}</span>
                             </Form.Item>
 
                             <Form.Item>
                                 <Input placeholder="Enter Your EmailId" name="email" value={userDetail.email}
                                     onChange={handleChange} addonBefore={<MailOutlined />} />
-
+                                <span className="danger">{error.email || ""}</span>
                             </Form.Item>
 
                             <Form.Item>
                                 Age : <InputNumber placeholder="age" name="age"
                                     onChange={value => handleChange({ target: { name: "age", value } })}
                                     value={userDetail.age} />
-
+                                <span className="danger">{error.age || ""}</span>
                             </Form.Item>
 
                             <Form.Item>
@@ -91,7 +138,7 @@ const UserContainer = (props) => {
                                     <Radio value="Female">Female</Radio>
                                     <Radio value="Other">Other</Radio>
                                 </Radio.Group>
-
+                                <span className="danger">{error.gender || ""}</span>
                             </Form.Item>
 
                             <Form.Item>
@@ -99,7 +146,7 @@ const UserContainer = (props) => {
                                 <Input.Password placeholder="Enter Your PassWord" name="password"
                                     value={userDetail.password} onChange={handleChange}
                                     addonBefore={(<LockOutlined />)} />
-
+                                <span className="danger">{error.password || ""}</span>
                             </Form.Item>
 
                             <Form.Item>
